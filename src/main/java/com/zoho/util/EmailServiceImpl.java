43e28c64170c44135d6f52cf836f03106bb11a7b
Your Namemdsajid940 <mdsajidali940@gmail.com>
2022-12-24 12:22:57 +0530

    zohocrmapp1 backup

---
+++ src/main/java/com/zoho/util/EmailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.zoho.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl implements EmailService {
+	
+	@Autowired
+	private JavaMailSender sendEmail;
+
+	@Override
+	public void sendEmail(String to, String subject, String message) {
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo(to);
+		mailMessage.setSubject(subject);
+		mailMessage.setText(message);
+		
+		sendEmail.send(mailMessage);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsps/menu.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<a href="create">Create new lead</a>
+	<a href="listLeads">Leads</a>
+	<a href="listContacts">Contacts</a>
+</body>
+</html>
